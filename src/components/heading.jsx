40954a76089eed0a9342b391bf1185875188604a
GuiCosta98 <guui.coosta@gmail.com>
2025-11-29 13:43:20 -0300

    feat: adiciona header e reorganiza estrutura de componentes

---
+++ src/components/heading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header h1">Keeper App</h1>
+    </header>
+  );
+}
+
+export default Header;
