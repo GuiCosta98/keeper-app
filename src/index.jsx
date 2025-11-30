234f6e5ffed27a8e97bda4e43479833d71f66a8c
GuiCosta98 <guui.coosta@gmail.com>
2025-11-29 22:37:49 -0300

    refactor: atualiza a estrutura dos componentes do app e index

--- src/index.jsx
+++ src/index.jsx
@@ -19,15 +19,12 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/heading";
+import App from "./components/app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
-root.render(<div>
+root.render(<App />);
 
-    <Header />
-
-</div>);
 
 
