748841d332248ad6b3ba237e27542ce5716611d6
GuiCosta98 <guui.coosta@gmail.com>
2025-12-10 18:52:14 -0300

    refactor: automatiza renderização de notas usando map()

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Footer from "./footer";
 import Header from "./header";
 import Note from "./note";
+import notes from "../notes";
+
+function createNote(newNote) {
+  return (
+    <Note key={newNote.key} title={newNote.title} content={newNote.content} />
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
   return (
     <div className="note">
-      <h1 className=".note h1">This is the note title</h1>
-      <p className=".note p">This is the note content</p>
+      <h1 className=".note h1">{props.title}</h1>
+      <p className=".note p">{props.content}</p>
     </div>
   );
 }
 
 export default Note;
+ 
